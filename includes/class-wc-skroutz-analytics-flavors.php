0b871dd050dce131fae578a585d54c8ccd35836e
Harris Kokkinos <harris.kokkinos@skroutz.gr>
2016-05-24 17:43:54 +0300

    Handle case where the shop has set zero tax
    If a shop has set in the vat woocommerce settings zero tax,
    then we calculate the tax based on the order revenue
    and the default country tax

--- includes/class-wc-skroutz-analytics-flavors.php
+++ includes/class-wc-skroutz-analytics-flavors.php
@@ -15,10 +15,13 @@ class WC_Skroutz_Analytics_Flavors
 {
 	const skroutz_analytics_url = 'https://analytics.skroutz.gr/analytics.min.js';
 	const skroutz_merchants_url = 'https://merchants.skroutz.gr/merchants/account/settings/analytics';
+  const skroutz_default_tax_rate = 24;
 
 	const alve_analytics_url = 'https://analytics.alve.com/analytics.min.js';
 	const alve_merchants_url = 'https://merchants.alve.com/merchants/account/settings/analytics';
+  const alve_default_tax_rate = 18;
 
 	const scrooge_analytics_url = 'https://analytics.scrooge.co.uk/analytics.min.js';
 	const scrooge_merchants_url = 'https://merchants.scrooge.co.uk/merchants/account/settings/analytics';
+  const scrooge_default_tax_rate = 20;
 }
