ef081a4057911ad1a8a7a33114f833a0e1ca7415
Harris Kokkinos <harris.kokkinos@skroutz.gr>
2016-05-25 14:55:48 +0300

    Bump to version 1.0.2

--- wc-skroutz-analytics.php
+++ wc-skroutz-analytics.php
@@ -15,7 +15,7 @@
  * Plugin Name:       WooCommerce Skroutz Analytics
  * Plugin URI:        https://github.com/skroutz/wc-skroutz-analytics
  * Description:       Integrate skroutz analytics to your WooCommerce enabled Wordpress site
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            Skroutz
  * Author URI:        www.skroutz.gr
  * License:           GPL-2.0
