0fdef0477e203a0f2596639d68854eb3cfac6489
Harris Kokkinos <harris.kokkinos@skroutz.gr>
2018-09-25 12:55:19 +0300

    Add global_object_name default value per flavor

--- includes/class-wc-skroutz-analytics-flavors.php
+++ includes/class-wc-skroutz-analytics-flavors.php
@@ -17,14 +17,17 @@ class WC_Skroutz_Analytics_Flavors
 	const skroutz_merchants_url = 'https://merchants.skroutz.gr/merchants/account/settings/analytics';
 	const skroutz_default_tax_rate = 24;
 	const skroutz_analytics_object = 'SkroutzAnalyticsObject';
+	const skroutz_global_object_name = 'skroutz_analytics';
 
 	const alve_analytics_url = 'https://analytics.alve.com/analytics.min.js';
 	const alve_merchants_url = 'https://merchants.alve.com/merchants/account/settings/analytics';
 	const alve_default_tax_rate = 18;
 	const alve_analytics_object = 'AlveAnalyticsObject';
+	const alve_global_object_name = 'alve_analytics';
 
 	const scrooge_analytics_url = 'https://analytics.scrooge.co.uk/analytics.min.js';
 	const scrooge_merchants_url = 'https://merchants.scrooge.co.uk/merchants/account/settings/analytics';
 	const scrooge_default_tax_rate = 20;
 	const scrooge_analytics_object = 'ScroogeAnalyticsObject';
+	const scrooge_global_object_name = 'scrooge_analytics';
 }
