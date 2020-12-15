b867a214bf6819b6ac3ba3341fb9fe4551e37764
Harris Kokkinos <harris.kokkinos@skroutz.gr>
2020-12-15 02:50:12 +0200

    Bump WooCommerce Tested up version to 4.8

--- wc-skroutz-analytics.php
+++ wc-skroutz-analytics.php
@@ -23,7 +23,7 @@
  * Text Domain:       wc-skroutz-analytics
  * Domain Path:       /languages
  * WC requires at least: 2.5.0
- * WC tested up to: 4.3
+ * WC tested up to: 4.8
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
